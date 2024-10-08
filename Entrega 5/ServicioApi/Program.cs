aa49681f622563e2660def54769e0991fe55c013
412061-ramiroibazeta <tu_correo@example.com412061@tecnicatura.frc.utn.edu.ar>
2024-10-08 02:15:45 -0300

    Usando proyecto de servicios, agrego turnosController

---
+++ Entrega 5/ServicioApi/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using ServicioBack.Data.Models;
+using ServicioBack.Data.Repositories;
+using ServicioBack.Data.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<TurnosDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IServicioRepository, ServicioRepository>();
+
+builder.Services.AddScoped<IServicioService, ServicioService>();
+
+builder.Services.AddScoped<ITurnoRepository, TurnoRepository>();
+
+//builder.Services.AddScoped<ITurnoService, TurnoService>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
